e03e29287c89f159277ded57ddab951e99e86231
Arnaud Wanet <andrewwanet9@gmail.com>
2023-01-25 22:33:07 +0100

    added the go crm app

---
+++ data-structures/webforms/crm_app.go
@@ -0,0 +1,96 @@
+package main  
+
+import (
+	"fmt" 
+	"net/http" 
+	"text/template" 
+	"log"
+)
+
+var template_html = template.Must(template.ParseGlob("templates/*")) 
+
+func Home(writer http.ResponseWriter, request *http.Request){
+	var customers []Customer 
+	customers = GetCustomers() 
+	log.Println(customers) 
+	template_html.ExecuteTemplate(writer, "Home", customers)
+}
+
+func Create(writer http.ResponseWriter, request *http.Request){
+	// var customers []Customer 
+	// customers = GetCustomers() 
+	// log.Println(customers) 
+	template_html.ExecuteTemplate(writer, "Create", nil)
+}
+
+func Insert(writer http.ResponseWriter, request *http.Request){
+	var customer Customer 
+	customer.CustomerName = request.FormValue("customername") 
+	customer.SSN = request.FormValue("ssn") 
+	var customers []Customer 
+	customers = GetCustomers() 
+	template_html.ExecuteTemplate(writer, "Home", customers)
+}
+
+func Alter(writer http.ResponseWriter, request *http.Request){
+	var customer Customer 
+	var customerId int 
+	var customerIdStr = request.FormValue("id") 
+	fmt.Scanf(customerIdStr, "%d", &customerId) 
+	customer.customerId = customerId 
+	customer.customerName = request.FormValue("customername") 
+	customer.SSN = request.FormValue("ssn") 
+	UpdateCustomer(customer) 
+	var customers []Customer 
+	customers = GetCustomers() 
+	template_html.ExecuteTemplate(writer, "Home", customers)
+}
+
+func Update(writer http.ResponseWriter, request *http.Request){
+
+	var customerId int 
+	var customerIdStr string 
+	customerIdStr = request.FormValue("id") 
+	fmt.Sscanf(customerIdStr, "%d", &customerId) 
+	var customer Customer
+	customer = GetCustomerById(customerId) 
+	template_html.ExecuteTemplate(writer, "update", customer)
+}
+
+func Delete(writer http.ResponseWriter, request *http.Request){
+	var customerId int 
+	var customerIdStr string 
+	customerIdStr = request.FormValue("id") 
+	fmt.Sscanf(customerIdStr, "%d", &customerId) 
+	var customer Customer 
+	customer = GetCustomerById(customerId) 
+	DeleteCustomer(customer) 
+	var customers []Customer 
+	customers = GetCustomers() 
+	template_html.ExecuteTemplate(writer, "Home", customers)
+}
+
+func View(writer http.ResponseWriter, request *http.Request){
+	var customerId int 
+	var customerIdStr string 
+	customerIdStr = request.FormValue("id") 
+	fmt.Sscanf(customerIdStr, "%d", &customerId) 
+	var customer Customer
+	customer = GetCustomerById(customerId) 
+	fmt.Println(customer) 
+	var customers []Customer 
+	customers = []Customer{customer} 
+
+	template_html.ExecuteTemplate(writer, "View", customers)
+}
+
+func main(){
+	log.Println("Server started on: http://localhost:8000") 
+	http.HandleFunc("/", Home) 
+	http.HandleFunc("/alter", Alter) 
+	http.HandleFunc("/update", Update) 
+	http.HandleFunc("/view", View) 
+	http.HandleFunc("/insert", Insert)  
+	http.HandleFunc("/delete", Delete) 
+	http.ListenAndServe(":8000", nil)
+}
---
+++ data-structures/webforms/crm_database_operations.go
@@ -0,0 +1,142 @@
+package main  
+
+import (
+	"database/sql" 
+	_"github.com/go-sql-dirver/mysql"
+)
+
+// Customer Class 
+type Customer struct{
+	CustomerId int 
+	CustomerName string 
+	SSN string 
+}
+
+// GetConnection method which returns sql.DB 
+func GetConnection() (database *sql.DB){
+	databaseDriver := "mysql" 
+	databaseUser :=  "newuser" 
+	databasePass := "newpass" 
+	databaseName := "crm" 
+	database, error := sql.Open(databaseDriver, 
+	databaseUser+":" + databasePass + "@/" + databaseName) 
+
+	if error != nil{
+		panic(error.Error())
+	}
+
+	return database 
+}
+
+// GetCustomerById with parameter customerId returns Customer 
+func GetCustomerById(customerId int) Customer{
+	var database *sql.DB 
+	database = GetConnection() 
+	var error error 
+	var rows *sql.Rows  
+	rows, error = database.Query("SELECT * FROM Customer WHERE CustomerId=?", customerId) 
+
+	if error != nil{
+		panic(error.Error())
+	}
+
+	var customer Customer 
+	customer = Customer{} 
+	for rows.Next(){
+		var customerId int 
+		var customerName string 
+		var SSN string 
+		error = rows.Scan(&customerId, &customerName, &SSN) 
+		if error != nil{
+			panic(error.Error())
+		}
+		customer.CustomerId = customerId 
+		customer.CustomerName = customerName 
+		customer.SSN = SSN
+	}
+
+	defer database.Close() 
+	return customer 
+}
+
+// GetCustomers method retuns Customer Array 
+func GetCustomers() []Customer{
+	var database *sql.DB 
+	database = GetConnection() 
+	var error error 
+	var rows *sql.Rows 
+	rows, error = database.Query("SELECT * FROM Customer ORDER BY Customerid DESC")
+	
+	if error != nil{
+		panic(error.Error())
+	}
+
+	var customer Customer 
+	customer = Customer{} 
+	var customers []Customer 
+	customers = []Customer{} 
+	
+	for rows.Next(){
+		var customerId int 
+		var customerName string 
+		var ssn string 
+		error = rows.Scan(&customerId, &customerName, &ssn) 
+		if error != nil{
+			panic(error.Error())
+		}
+		customer.CustomerId = customerId 
+		customer.CustomerName = customerName 
+		customer.SSN = ssn 
+		customers = append(customers, customer) 
+	}
+
+	defer database.Close() 
+	return customers 
+}
+
+func InsertCustomer(customer Customer){
+	var database *sql.DB 
+	database = GetConnection() 
+	var error error 
+	var insert *sql.Stmt 
+	insert, error = database.Prepare("INSERT INTO CUSTOMER(CustomerName, SSN) VALUES(?,?)") 
+	if error != nil{
+		panic(error.Error())
+	}
+
+	insert.Exec(customer.CustomerName, customer.SSN) 
+	defer database.Close() 
+}
+
+// the UPdateCustomer method prepares the Update statement by passing 
+// the CustomerName and SSN from the customer object; this is shown in 
+// the following code 
+// Update Customer method 
+func UpdateCustomer(customer Customer){
+	var database *sql.DB 
+	database = GetConnection() 
+	var error error 
+	var update *sql.Stmt 
+	update, error = database.Prepare("UPDATE CUSTOMER SET CustomerName=? WHERE CustomerId=?") 
+	if error != nil{
+		panic(error.Error())
+	}
+
+	update.Exec(customer.CustomerName, customer.SSN, customer.CustomerId) 
+	defer database.Close()
+}
+
+// DeleteCustomer method 
+func DeleteCustomer(customer Customer){
+	var database *sql.DB 
+	database = GetConnection() 
+	var error error  
+	var delete *sql.Stmt 
+	delete, error = database.Prepare("DELETE FROM Customer WHERE CustomerId=?") 
+	if error != nil{
+		panic(error.Error())
+	}
+
+	delete.Exec(customer.CustomerId) 
+	defer database.Close() 
+}
